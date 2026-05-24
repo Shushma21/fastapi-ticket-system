4d44126ed8b0b9d61343bba98393c84e0d5238bc
Shushma21 <shushma972001@gmail.com>
2026-05-24 20:12:08 +0530

    Configured environment variables using python-dotenv

--- app/auth.py
+++ app/auth.py
@@ -14,7 +14,7 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 if not SECRET_KEY:
     raise RuntimeError("SECRET_KEY environment variable is not set")
-ALGORITHM = "HS256"
+ALGORITHM = os.getenv("ALGORITHM")
 
 pwd_context = CryptContext(
 	schemes = ["bcrypt"],
